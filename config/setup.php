59cf8b0001c8a107a19467034a42ca1aa3fab856
kyazd <kyazdani@e3r1p9.42.fr>
2018-04-13 20:00:41 +0200

    init

---
+++ config/setup.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+if (!isset($_SESSION['log_user']))
+    $_SESSION['log_user'] = "";
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once('config/setup.php');
+?>
+<!doctype html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <title>Camagru</title>
+        <link rel="stylesheet" href="style/style.css">
+	</head>
+	<body>
+		<header>
+            <div class="headerBox">
+                <li class="headList">
+                    <ul>1</ul>
+                    <ul>2</ul>
+                    <ul>3</ul>
+                </li>
+                <div>
+                    <h1>Camagru</h1>
+                    <h3>Take a pic, pimp it, and get the fame you deserve</h3>
+                </div>
+			    <span class="logBox"><?php if (isset($_SESSION['log_user']) && $_SESSION['log_user'] !== "") { echo "Account"; } else { echo "Connexion"; } ?></span>
+            </div>
+		</header>
+		<div class="main">
+			<div class="main_main">
+			</div>
+			<div class="nav">
+			</div>
+		</div>
+		<footer>
+		</footer>
+	</body>
+</html>
