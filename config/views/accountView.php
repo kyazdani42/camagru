34d3cf67a782af54495a5b9b0c5a07afdfac3c95
kyazd <kyazdani@e1r1p9.42.fr>
2018-04-19 19:55:12 +0200

    forms in account

--- config/views/accountView.php
+++ config/views/accountView.php
@@ -1,6 +1,24 @@
-<a href="<?= URL ?>/Account/delete">Delete your account</a>
-<a href="<?= URL ?>/Account/modPass">Change your password</a>
-<a href="<?= URL ?>/Account/modLog">Change your login</a>
-<a href="<?= URL ?>/Account/modEmail">Change your email</a>
-<!--<a href="<?= URL ?>/Account/delete">Describe yourself</a>-->
+<div class="contentAccount">
+
+    <form action="<?= URL ?>Account/modLog">
+        <input type="text" name="newLogin" value="">
+        <input type="submit" value="Change your login">
+    </form>
+
+    <form action="<?= URL ?>Account/modPass">
+        <input type="text" name="newPass" value="">
+        <input type="submit" value="Change your password">
+    </form>
+
+    <form action="<?= URL ?>Account/modEmail">
+        <input type="text" name="newEmail" value="">
+        <input type="submit" value="Change your email">
+    </form>
+
+    <a href="<?= URL ?>Account/delete">Delete your account</a>
+    <!--<a href="<?= URL ?>Account/describe">Describe yourself</a>-->
+    <!--<a href="<?= URL ?>Account/addPic">Add a profile pic</a>-->
+</div>
+
+
 
