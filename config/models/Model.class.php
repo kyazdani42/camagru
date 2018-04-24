d769bd099c25d4d3406a9f0d65db0b65fa73603a
kyazdani42 <kyazdani@student.42.fr>
2018-04-24 14:36:23 +0200

    corrected Model class query select

--- config/models/Model.class.php
+++ config/models/Model.class.php
@@ -5,7 +5,7 @@ Abstract class Model {
     private static $_db;
 
     protected function request($sql, $params = null) {
-        if ($params = null) {
+        if ($params === null) {
             $result = self::_getDb()->query($sql);
         } else {
             $result = self::_getDb()->prepare($sql);
@@ -16,11 +16,11 @@ Abstract class Model {
 
     private function _getDb() {
 
-        if (self::$_db == null) {
+        if (self::$_db === null) {
             $dsn = Config::getConf("dsn");
             $user = Config::getConf("user");
             $pass = Config::getConf("pass");
-            self::$_db = new PDO($dsn, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+           	self::$_db = new PDO($dsn, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         }
         return self::$_db;
     }
