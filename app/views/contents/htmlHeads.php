8879d702c3b90634a33e2a8224daf639a2f18ce2
kyazdani42 <kyazdani@student.42.fr>
2018-05-13 18:24:51 +0200

    header is fixed on top of page, might wanna add js to remove title on scroll, added meta viewport in html

--- app/views/contents/htmlHeads.php
+++ app/views/contents/htmlHeads.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camagru - <?= $title ?></title>
     <link rel="stylesheet" href="public/style/main.css">
     <link rel="stylesheet" href="public/style/heart.css">
