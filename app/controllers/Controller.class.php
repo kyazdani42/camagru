e30bd2e5597a03c311aebddb622f0899c9bef0d1
kyazdani42 <kyazdani@student.42.fr>
2018-05-13 17:47:01 +0200

    added protected function to check inputs in controller. Need to implement everywhere

--- app/controllers/Controller.class.php
+++ app/controllers/Controller.class.php
@@ -5,13 +5,31 @@ abstract class Controller {
     protected $_view;
 
     public function __construct() {
+
         $this->_view = new View();
     }
 
 	protected function _isAjax() {
+
 		if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
 			return (1);
 		else
 			return (0);
+	}
+
+	protected function _checkInput(array $kwargs) {
+
+		if (isset($kwargs['ascii'])) {
+			if (preg_match("/[^0-9a-zA-Z]/", $kwargs['ascii'])) {
+				return (0);
+			} else
+				return (1);
+		}
+		else if (isset($kwargs['mail'])) {
+			if (preg_match("/[0-9a-zA-Z]+@[a-z]+\.[a-z].{1,2}/", $kargs['mail'])) {
+				return (1);
+			} else
+				return (0);
+		}
 	}
 }
--- app/controllers/visitor/SetController.class.php
+++ app/controllers/visitor/SetController.class.php
@@ -36,5 +36,4 @@ class SetController extends Controller {
 
    }
 
-
 }
