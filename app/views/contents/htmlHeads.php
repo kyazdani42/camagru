cc9c1d5699d4bb64f1dcdd736fa90d3ff21acd8e
kyazdani42 <kyazdani@student.42.fr>
2018-05-18 12:31:53 +0200

    added styles for account modifications and splitted into multiple css files

--- app/views/contents/htmlHeads.php
+++ app/views/contents/htmlHeads.php
@@ -5,6 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camagru - <?= $title ?></title>
     <link rel="stylesheet" href="public/style/main.css">
+    <link rel="stylesheet" href="public/style/header.css">
+    <link rel="stylesheet" href="public/style/forms.css">
+    <link rel="stylesheet" href="public/style/home.css">
     <link rel="stylesheet" href="public/style/heart.css">
     <link rel="icon" href="public/images/favicon.ico" >
 </head>
--- app/views/logged/accountView.php
+++ app/views/logged/accountView.php
@@ -1,22 +1,22 @@
 <div class="contentAccount">
 
-    <form action="<?= URL ?>Account/modLog" method="post">
+    <form class="modifySettings" action="<?= URL ?>Account/modLog" method="post">
         <input type="text" name="newLogin" value="">
         <input type="submit" value="Change your login">
     </form>
 
-    <form action="<?= URL ?>Account/modPass" method="post">
+    <form class="modifySettings" action="<?= URL ?>Account/modPass" method="post">
         <input type="password" name="newPass" value="">
         <input type="submit" value="Change your password">
     </form>
 
-    <form action="<?= URL ?>Account/modEmail" method="post">
+    <form class="modifySettings" action="<?= URL ?>Account/modEmail" method="post">
         <input type="email" name="newEmail" value="">
         <input type="submit" value="Change your email">
     </form>
 
-    <a id="mailChecker" href="<?= URL ?>Account/setMailCheck"><?php if ($array['check'] === '0') { ?>enable<?php } else { ?>disable<?php } ?> mail when someone comments one of your pictures</a>
-    <a href="<?= URL ?>Account/delete">Delete your account</a>
+    <a class="mailChecker" id="mailChecker" href="<?= URL ?>Account/setMailCheck"><?php if ($array['check'] === '0') { ?>enable<?php } else { ?>disable<?php } ?> mail when someone comments one of your pictures</a>
+    <a class="delAccount" href="<?= URL ?>Account/delete">Delete your account</a>
 </div>
 
 <script src="public/js/accountMailCheck.js"></script>
