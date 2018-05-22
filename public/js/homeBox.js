161ebb3141f569bc43f1d73d4ee46ec82825b776
kyazdani42 <kyazdani@student.42.fr>
2018-05-22 15:46:18 +0200

    modalizing home boxes

--- public/js/homeBox.js
+++ public/js/homeBox.js
@@ -119,4 +119,18 @@ if (ur !== undefined) {
             });
         }
     }
+
+}
+const clicks = document.getElementsByClassName("openComs");
+const hidden = [];
+for (let i = 0; i < clicks.length; i++) {
+    hidden[i] = clicks[i].parentNode.nextElementSibling;
+    clicks[i].addEventListener("click", () => {
+        hidden[i].style.display = "block";
+    });
+    window.addEventListener("click", (e) => {
+        if (e.target === hidden[i]) {
+            hidden[i].style.display = "none";
+        }
+    });
 }
